bc7e8dd06332d7421381c7ff37323e676de9d141
Felipe Gama <felipegamae2@gmail.com>
2026-03-11 16:08:23 -0300

    add simple linear model evaluation loop using random weight and training dataset

--- main.c
+++ main.c
@@ -10,6 +10,8 @@ float train[][2] = {
     {4, 8},
 };
 
+#define train_count sizeof(train) / sizeof(train[0]) // train_count is the number of lines that there are in train
+
 float rand_float(void)
 {
     return (float)rand() / (float)RAND_MAX;
@@ -21,8 +23,23 @@ int main()
 
     // y = x * w -> mult of parameters
 
-    float w = rand_float();
-    printf("%f\n", w);
+    float w = rand_float() * 10.0f;
+
+    printf("Tha weith 'w': %f\n", w);
+
+    float acumulate_of_distance_between_y_e_trainoutputs = 0.0f;
+    for (size_t i = 0; i < train_count; ++i)
+    {
+        float x = train[i][0];
+        float y = x * w; // on this case, y is equals train[i][1] (the output of train)?
+
+        // before, we need to look to the perfomance of this model rigth now, so we'll cacth the distance between y and the outputs from train (train[i][0]):
+        float distance = y - train[i][1];
+        acumulate_of_distance_between_y_e_trainoutputs += distance * distance;
+
+        // To know that, we'll compare y and the outputs of train:
+        printf("actual: %f | expected: %f\n", y, train[i][1]);
+    }
 
     return 0;
 }
